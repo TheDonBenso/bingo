3dd66be987ca45c0cd7bf3fa63e350cdafe6cbe7
TheDonBenso <b.m.nyakabwa@gmail.com>
2018-10-14 14:56:19 +0200

    UI Update

---
+++ app.js
@@ -0,0 +1,36 @@
+/* 
+setup MVC style
+*/
+
+var BingoController = (function(){
+//Populate Cards
+
+//populate rolling basket with 90 balls 
+
+//setup arrays 
+//3 arrays for each card
+//
+
+
+});
+
+var UIController = (function(){
+//getDomStrings
+//update cards
+
+});
+
+var GlobalController= (function(BingoController, UIController){
+//setup eventlisteners
+
+
+return {
+    init: function() {
+
+    }
+}
+});
+
+/*
+Initialize the controller
+*/
