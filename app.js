c8bb8e9989f9470eba9d033e65154d11bfbbd6cc
TheDonBenso <b.m.nyakabwa@gmail.com>
2018-10-28 22:56:53 +0200

    trouble with binding controllers

--- app.js
+++ app.js
@@ -8,22 +8,17 @@ var BingoController = (function(){
 //populate rolling basket with 90 balls 
 var data = {
 
-    ballRoller : [1,2,3,4,5,6,7,8,9,10,
-                    11,12,13,14,15,16,17,18,19,20,
-                        21,22,23,24,25,26,27,28,29,30,
-                            31,32,33,34,35,36,37,38,39,40,
-                                41,42,43,44,45,46,47,48,49,50,
-                                    51,52,53,54,55,56,57,58,59,60,
-                                        61,62,63,64,65,66,67,68,69,70, 
-                                            71,72,73,74,75,76,77,78,79,80,
-                                                81,82,83,84,85,86,87,88,89,90],
+    ballRoller : [0,1],
+    pickedBalls : [0,1],
 
-    pickedBalls : [],
+    ticketpopulator :   [   [0,1], //deck1
+                                [0,1], //deck2
+                                    [0,1] //deck3
+                        ],
 
-    ticketpopulator :   [   [], //deck1
-                                [], //deck2
-                                    [] //deck3
-                        ]
+    getballroller: function(){
+        return this.ballroller;
+    }
 };
 //setup arrays 
 
@@ -32,6 +27,13 @@ var data = {
 
 return {
 
+    LoadRoller : function(){
+
+        for(i=1; i<=90; i++)
+        {
+            data.ballRoller[i] = i;
+        }
+    }, 
 
     SetRandomPick : function(){
       var pick
@@ -42,6 +44,7 @@ return {
             {
                 data.pickedBalls.push(pick);
                 data.ballRoller.splice(pick+1,1);
+                break;
              }
         }
         while(1 < pick < 90);
@@ -49,10 +52,44 @@ return {
 
     },
 
-    CheckTicketLine: function(num){},
+    CheckTicketLine: function(num){
 
 
-    
+    },
+
+    GetBallRoller: function(){
+        return {
+            ballRoller: data.ballRoller
+        }
+    },
+
+    LoadDecks : function(){
+        var randomTicket = [];
+        
+        for(x = 0; x<18; x++)
+        {
+             var pick = Math.random() * 100;
+            
+            if(1<=pick<=90)
+            {            
+  
+               if( (x+1) % 3 === 0 ){
+                   data.ticketpopulator[2].map(pick).sort();
+               } else if ((x+1)%2 ===0){
+                   data.ticketpopulator[1].map(pick).sort();
+               } else {
+                   data.ticketpopulator[0].map(pick).sort();
+               }
+
+            }
+        }
+       
+
+
+
+        console.log(randomTicket);
+        
+    }
 
 
 };
@@ -72,9 +109,22 @@ var UIController = (function(){
         //update cards
 
         return {
+            displayNumbers: function(rollers){
+                var str_rollers = "";
+                for(x=1; x<=90;x++)
+                {
+                    str_rollers += rollers.ballRoller[x]+", "
+                    //displayontohtml
+                }
+
+                document.querySelector('.bingo_roller_numbers').textContent = str_rollers;
+            },
+
             getDOMstrings: function(){
                 return domstrings;
             }
+
+
         };  
        
         
@@ -86,12 +136,12 @@ var GlobalController= (function(BCtrl, UICtrl){
 
     var setupListeners = function(){
 
-        var dom = UICtrl.getDOMstrings; 
+       // var dom = UICtrl.getDOMstrings(); 
+        
 
-        document.querySelector(UICtrl.domstrings.btnstart).addEventListener('click',startGame);
-         
-        document.querySelector(UICtrl.domstrings.btn_start_decks).addEventListener('click',loadDecks);
-
+        document.querySelector('.btn_start').addEventListener('click',startGame);
+ 
+        document.querySelector('.btn_start_decks').addEventListener('click',loadDecks);
     };
 
     var loadDecks = function(){
@@ -100,19 +150,29 @@ var GlobalController= (function(BCtrl, UICtrl){
 
     var startGame = function(){
         //randomize each players deck
+        BCtrl.LoadRoller;
+        BCtrl.loadDecks;
+
+        var getballroller = BCtrl.data.getballroller;
+        var getboundballroller = getballroller.bind(BCtrl.data);
+        UICtrl.displayNumbers(getboundballroller)
         //place results onto the decks
         //initialize the random number generator
         //start picking your random numbers
         //restyle picked numbers from the decks
-        alert("ready!");
+        console.log(BCtrl.testingBall);
+       // BCtrl.GetBallRoller.ballRoller = [];
+      //  console.log(BCtrl.GetBallRoller.ballRoller);
+       
     };
 
 
-return {
-    init: function() {
+    return {
+        init: function() {
+            
             setupListeners();
+        }
     }
-}
 })(BingoController, UIController);
 
 /*
